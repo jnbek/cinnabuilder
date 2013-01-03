a158eb0fdec0f3dc4686d686c089ff02ad750fba
John D Jones III <jnbek1972@gmail.com>
2013-01-02 21:18:49 -0700

    Add a force opt

--- build_pkgs.pl
+++ build_pkgs.pl
@@ -10,13 +10,17 @@ $s->main();
 sub main {
     my $self = shift;
     #--no_skip builds all the packages regardless of if they have an upgrade.
-    my ( $no_skip, $help, $do_repo_add ) = '';
+    my ( $no_skip, $help, $do_repo_add, $force ) = '';
     my $opts = GetOptions(
         "no_skip"     => \$no_skip,
         "do_repo_add" => \$do_repo_add,
+        "force"       => \$force,
         "help|h"      => \$help,
     );
     return $self->usage if $help;
+    if ($force) {
+        $force = "-f";
+    }
     my $home              = $ENV{'HOME'};
     my $export_dir        = "$home/build/AUR";
     my $package_dir       = "$home/build/PACKAGES";
@@ -129,7 +133,7 @@ sub main {
         my $current_local = $self->current_local($package);
         print "$package: AUR: $current_aur LOCAL: $current_local\n";
         next unless ( ($no_skip) || ( !$current_local || $current_aur gt $current_local ) );
-        my $return = system( "/usr/bin/yaourt -S --export $export_dir $package" );
+        my $return = system( "/usr/bin/yaourt -S $force --export $export_dir $package" );
         if ( $return != 0 ) {
             push @aur_fail, $package;
         }
@@ -139,7 +143,7 @@ sub main {
         my $current_local = $self->current_local($package);
         print "$package: AUR: $current_aur LOCAL: $current_local\n";
         next unless ( $no_skip || ( !$current_local || $current_aur gt $current_local ) );
-        my $return = system( "/usr/bin/yaourt -S --export $export_dir --noconfirm $package" );
+        my $return = system( "/usr/bin/yaourt -S $force --export $export_dir --noconfirm $package" );
         if ( $return != 0 ) {
             push @aur_fail, $package;
         }
@@ -195,6 +199,7 @@ sub usage {
     Options:
         --no_skip:      Build all packages even if no upgrade is available
         --do_repo_add:  Execute repo_add in \$package_dir
+        --force:        Pass along -f to yaourt --export to force package overwrite for same version.
         --help or -h:   Show this menu
     Example: $0 --no_skip --do_repo_add
     };
